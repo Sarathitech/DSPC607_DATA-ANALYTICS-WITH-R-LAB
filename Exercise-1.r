92a2fc2e30f3e318d27902ae1fafdf95d8fda6e9
RAGUPATHI-M <ragumdr812@gmail.com>
2022-02-23 16:48:26 +0530

    R

---
+++ Exercise-1.r
@@ -0,0 +1,153 @@
+#Learning R Programming Basic Commends
+
+1+1		       #Addition
+10-2		     #Subtraction
+2*2	         #Multiplication
+100/10	     #Division
+10^2         #Exponentiation
+
+abs(-10)     #abs function in R
+ceiling(4.5) #Ceiling and Floor Function in R
+floor(4.5)  
+sqrt(10)     #Square Root Fun in R
+exp(2)       #Exponentiation Fun in R
+pi           #Print pi value.
+log(100)		 #natural logs(i.e. base e)
+log(100, base=10)	#base 10 logs
+
+(2+2)*2      #Expression
+1+(2*2)
+2==2         # Relational operation
+5<=6
+5>=6
+
+x<- c(2,5,7,8,9) #Creating a Vector with integer vales using c fun in R
+x > 5
+mean(c(1,2,3,4))        #Return mean value
+x<- c("apple","banana") #Creating a Vector with strings using c fun in R
+x
+
+Y<- 10+10
+Z<- -10+10
+Y;Z
+x<-c(1,2,3)
+y<-rnorm(4,5)            #random deviation function
+x;y
+
+getwd()                  #Current working directory
+search()                 #base packages
+x<-1:10                  #creating a vector with 1 to 10.
+x
+ls( )                    #list the used variables
+
+save(x, y, file = "xy.RData")# x and y values are printed in a file called xy.RData.
+save.image()                 # creating ".RData" in current working directory
+load("xy.RData")             # Loading  "xy.RData" in current working directory
+unlink("xy.RData")
+
+lsx<-c(TRUE,FALSE)       	   #logical vector
+y<-c("a","b","c")	           #character vector
+z<-c(1,2.3,4)	               #numeric integer vector
+m<-c(1.2,1.5,1.7)	           #numeric real/double vector
+lsx; y; z; m
+class(x); typeof(x); mode(x)
+
+is.numeric(c("a","b"))       #Test whether the given data are numeric or not
+is.numeric(c(5,6))
+as.character(c(1,2,3))       #Converted the given into character form 
+as.numeric(c("c","4","b"))   #Converted the given into numeric form 
+
+sum(c(FALSE,TRUE,TRUE))
+c(1,2,3,4,5,6,7,8)
+1:10
+seq(1,8, by =2)              #Generates the sequence from:to by
+rep(1,5)	                   #Replicate Elements of Vectors and Lists
+rep(c(1,2,3),5)
+
+x<-c(1,2,3,4,5)
+names(x)<-c("a","b","c","d","e") #Names of x
+x
+
+x[c(1,2,3)]	 #by numeric position
+x[x<3]	     #by logical vector
+x
+
+d=x[x<3]	   #by logical vector
+d
+x[c("b","c")]	 # by name
+f=x[c("b","c")]
+f
+
+y<-matrix(c(1,2,3,4,5,6),byrow=TRUE,ncol=2) #Creating a matrix
+y          
+class(y)
+dim(y)	
+nrow(y)	#no of rows
+ncol(y)	#no of column
+rownames(y)<-c("a","b","c")
+colnames(y)<-c("col1","col2")
+y                            
+y["a",]	
+y[c(1,2),]
+
+#Creating a list
+x = list(name="Arun Patel", nationality="Indian", height=5.5, grades=c(95,45,80))
+x
+class(y)
+x$name
+x$hei
+
+#Creating a Data frame
+z<-data.frame(var1=1:9,var2=letters[1:9])
+z
+View(z)
+
+#Reading the Data from csv file
+data=read.csv("d:\\sample.csv",header=T,sep=",")
+data
+nrow(data)
+ncol(data)
+data
+head(data)
+
+#Creating a function 
+hw.f1 <- function()
+{
+  hw <- "Hello World"
+  hw
+}
+hw.f1()
+
+
+Install.packages("Matrix")
+dependencies=TRUE 
+Install.packages("Matrix", dependencies=TRUE)
+d=library(Matrix)
+d
+
+x<-c(1,2,NA,4)
+x
+x<-c("a","b",NA,"c")
+x
+is.na(x)
+na.omit(x)
+
+
+library(MASS)	#user survey data from MASS package
+data(survey)	#load an internal data set
+data()
+mydata<-survey
+names(mydata)  
+str(mydata)
+
+
+x <- c(1,2,3,4,5)
+range(x)
+dat<-data.frame(x=c(1,2,3,4,5),y=c(1,1,0,1,1))
+dat
+dat$z<-dat$x +dat$y
+dat
+dat$z<-dat$x+10
+
+rm(x)                    #removing a variable 
+x
---
+++ Exercise-2.R
@@ -0,0 +1,120 @@
+# Matrices and Arrays
+a<- matrix(data = c(1,2,3),nrow=1,ncol=3)
+a
+a<- matrix(data = c(1,2,3),nrow=3,ncol=1)
+a
+
+#Filling Direction
+matrix(data=c(1,2,3,4,5,6),nrow =2,ncol=3,byrow=FALSE)
+matrix(data=c(1,2,3,4,5,6),nrow =2,ncol=3,byrow=TRUE)
+
+#Row and Column Bindings
+rbind(1:3,4:6)
+cbind(c(1,4),c(2,5),c(3,6))
+
+#String on Matrix
+Names <- matrix(data=c("R","V","A","E","G","L","U","S"),nrow =4,ncol=2,byrow=TRUE)
+Names
+
+#Matrix Dimensions
+mymat <- rbind(c(1,2,3),5:3,c(100,20,30),12:14)
+dim(mymat)
+nrow(mymat)
+ncol(mymat)
+print(mymat)
+
+#Subsetting
+A<-matrix(data=c(1,2,3,4,5,6,7,8,9),nrow=3,ncol=3,byrow =TRUE )
+A
+A[3,3]
+
+#Row, Column, Diagonal Extractions
+A[,1]
+
+A[2,]
+
+A[1:2,]
+A[,1:2]
+A[1:2,1:2]
+
+A[,c(3,1)]
+A[c(1,3),c(2)]
+A[c(2,1),1:2]
+
+diag(x=A)
+
+#Omiting and Overwriting
+A[,-2]
+A[-1,2:3]
+A[-1,-2]
+A[-2,-c(2,3)]
+
+B<-A
+B
+B[2,]<-1:3
+B
+B[c(1,2),2] <- 100
+B
+
+diag(x=B)<- rep(x=0,times=3)
+B
+
+#Matrix operations and Algebra
+#Matrix Transpose
+A <- rbind(1:3,4:6)
+A
+t(A)
+t(t(A))
+t(t(A)) == A
+
+#Identity Matrix
+A<-diag(x=3)
+A
+
+#Scalar multiple of a Matrix
+A<-cbind(c(1,2,3),c(4,5,6))
+A
+a<-3
+a*A
+
+#Matrix Addition and Subtraction
+A<-matrix(data=c(1,2,3,4,5,6),nrow=3,ncol=2,byrow=FALSE)
+A
+B<-A
+B
+A+B
+A-B
+
+#Matrix Multiplication
+A<-rbind(1:3,c(1,4,5))
+A
+B<-t(A)
+dim(A)
+dim(B)
+A%*%B
+B%*%A
+
+#Matrix Inversion
+A<-cbind(1:2,4:5)
+A
+solve(A)
+
+A%*%solve(A)
+
+
+#Multidimensional Arrays
+AR<-array(data=1:24,dim=c(3,4,2))
+AR
+BR<- array(data=rep(1:24,times=4),dim=c(3,4,2,3))
+BR
+
+#Subsets and Extraction
+AR[2,,2]
+AR[1,, ]
+
+BR[1,,,1]
+BR[,,2,]
+BR[3:2,4,,]
+BR[2,,1,]
+
+
