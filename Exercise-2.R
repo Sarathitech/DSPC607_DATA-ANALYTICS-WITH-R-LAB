439fcab2f05d4e0cf46fe39226f6fd65e244c5e8
RAGUPATHI-M <ragumdr812@gmail.com>
2022-02-26 08:54:49 +0530

    R

--- Exercise-2.R
+++ Exercise-2.R
@@ -1,135 +1,103 @@
-# Matrices and Arrays
-a<- matrix(data = c(1,2,3),nrow=1,ncol=3)
-a
-a<- matrix(data = c(1,2,3),nrow=3,ncol=1)
-a
+# 2 a) Write a R program to perform the Matrix Operations such as Addition (+), Subtraction (-), Multiplication (%*%), Transpose (t), Inverse (solve()) and Diagonal of a Matrix (diag) using matrix, rbind and cbind  commands
 
-#Filling Direction
-matrix(data=c(1,2,3,4,5,6),nrow =2,ncol=3,byrow=FALSE)
-matrix(data=c(1,2,3,4,5,6),nrow =2,ncol=3,byrow=TRUE)
+#Creating A and B Matrices using matrix command
+A <- matrix(data = c(1,2,3,4),nrow = 2,ncol = 2, byrow = T)
+B <- matrix(data= c(5,6,7,8),nrow = 2,ncol=2,byrow = T)
 
-#Row and Column Bindings
-rbind(1:3,4:6)
-cbind(c(1,4),c(2,5),c(3,6))
+#Creating C and D Matrices using rbind and cbind commands
+C <- rbind(c(1,2,3),c(6,5,4))
+D <- cbind(c(9,5,1),c(3,2,7))
 
-#String on Matrix
-Names <- matrix(data=c("R","V","A","E","G","L","U","S"),nrow =4,ncol=2,byrow=TRUE)
-Names
-
-#Matrix Dimensions
-mymat <- rbind(c(1,2,3),5:3,c(100,20,30),12:14)
-dim(mymat)
-nrow(mymat)
-ncol(mymat)
-print(mymat)
-
-#Subsetting
-A<-matrix(data=c(1,2,3,4,5,6,7,8,9),nrow=3,ncol=3,byrow =TRUE )
 A
-A[3,3]
-
-#Row, Column, Diagonal Extractions
-A[,1]
-
-A[2,]
-
-A[1:2,]
-A[,1:2]
-A[1:2,1:2]
-
-A[,c(3,1)]
-A[c(1,3),c(2)]
-A[c(2,1),1:2]
-
-diag(x=A)
-
-#Omiting and Overwriting
-A[,-2]
-A[-1,2:3]
-A[-1,-2]
-A[-2,-c(2,3)]
-
-B<-A
 B
-B[2,]<-1:3
-B
-B[c(1,2),2] <- 100
-B
+C
+D
 
-diag(x=B)<- rep(x=0,times=3)
-B
+print("Resultant Matrices")
 
-#Matrix operations and Algebra
-#Matrix Transpose
-A <- rbind(1:3,4:6)
-A
-t(A)
-t(t(A))
-t(t(A)) == A
-
-#Identity Matrix
-A<-diag(x=3)
-A
-
-#Scalar multiple of a Matrix
-A<-cbind(c(1,2,3),c(4,5,6))
-A
-a<-3
-a*A
-
-#Matrix Addition and Subtraction
-A<-matrix(data=c(1,2,3,4,5,6),nrow=3,ncol=2,byrow=FALSE)
-A
-B<-A
-B
+print("Addition of Two Matrices=")   
 A+B
+
+print("Subtraction of Two Matrices=")
 A-B
 
-#Matrix Multiplication
-A<-rbind(1:3,c(1,4,5))
-A
-B<-t(A)
-dim(A)
-dim(B)
-A%*%B
-B%*%A
+print("Scalar Multiplication of A Matrix=")
+3*A
 
-#Matrix Inversion
-A<-cbind(1:2,4:5)
-A
-solve(A)
+print("Multiplication of Two Matrices C & D=")
+C%*%D
 
-A%*%solve(A)
+print("Diagonal Matrix of A")
+diag(A)
+
+cat("Transpose of C",t(C))
 
 
-#Multidimensional Arrays
-AR<-array(data=1:24,dim=c(3,4,2))
-AR
-BR<- array(data=rep(1:24,times=4),dim=c(3,4,2,3))
-BR
+cat("Inverse of B",solve(B))
 
-#Subsets and Extraction
-AR[2,,2]
-AR[1,, ]
 
-BR[1,,,1]
-BR[,,2,]
-BR[3:2,4,,]
-BR[2,,1,]
 
-rows <- as.integer(readline("Enter the number of rows: ")) 
-columns <- as.integer(readline("Enter the number of columns: "))
-total<-rows*columns
-cat("Total number elements",total)
-i <- 1
-j <- 0
-while(j<=total){
- cat("Element",i)
- r<-as.integer(readline("Waiting for input: ")) 
- x <- list()
- append(x,r)
- i= i+1
- j = j+1
+# 2b) Write a R program to perform the Matrix Operations such as Addition (+), Subtraction (-), Multiplication (%*%), Transpose (t), Inverse (solve()) and Diagonal of a Matrix (diag) using vector and matrix. (Get the input matrix from Keyboard)
+
+print("Enter the size of matrix: A")
+rows <- as.integer(readline("Rows: "))
+columns <-as.integer(readline("Columns: "))
+dimension1 <- rows*columns
+avec1 = vector(mode="integer",length = 0)
+index = 1
+for(i in 1:dimension1)
+{
+  cat("Enter the element: ",index)
+  value1 = as.integer(readline())
+  avec <- c(avec,value1)
+  index = index + 1
 }
-mat1 <- matrix(data =x ,nrow=rows,ncol = columns, byrow = T ) 
-mat1
+
+A<- matrix(data=avec, nrow =rows,ncol =columns,byrow=T)
+
+
+
+print("Enter the size of  matrix:B")
+rows <- as.integer(readline("Rows: "))
+columns <-as.integer(readline("Columns: "))
+dimension2 <- rows*columns
+avec2 = vector(mode="integer",length = 0)
+index = 1 
+for(i in 1:dimension2)
+{
+  cat("Enter the element: ",index)
+  value2 = as.integer(readline())
+  avec2 <- c(avec,value2)
+  index = index + 1
+}
+
+B<- matrix(data=avec2, nrow =rows,ncol =columns,byrow=T)
+
+print("Matrix of A")
+A
+
+print("Matrix of B")
+B
+
+cat("Resultant Matrices")
+
+print("Addition of Two Matrices=")
+A+B
+
+print("Subtraction of Two Matrices=")
+A-B
+
+print("Scalar Multiplication of a Matrix=")
+3*A
+
+print("Multiplication of Two Matrices=")
+A%*%B
+
+print("Diagonal Matrix")
+diag(A)
+
+cat("Transpose of A",t(A))
+
+cat("Inverse of A",solve(A))
+
+                                                                        
