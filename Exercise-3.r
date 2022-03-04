041888b89cfac910d3f3e24a3a0cf4e5690f5581
ragu-12 <ragumdr812@gmail.com>
2022-03-04 11:14:35 +0530

    R

---
+++ Exercise-3.r
@@ -0,0 +1,8 @@
+#Ex3a. Create a data frame called student data, explore the structure of the data and process it using data.frame.
+
+sdata <- data.frame(sname = c("ragu","vicky","vel"),srollno=c(12,41,05),sbranch=c("ds","ds","ds"),cgpa=c(8.85,9.12,7.45))
+sdata
+
+head(sdata)
+nrow(sdata)
+ncol(sdata)
